58f0e8679f21464261921cc1a8e4a2b93f3b4c8e
dev92r <dev92r@gmail.com>
2026-03-02 23:12:25 +0530

    features added

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
-import { getFeatures, getHeroContent } from "@/lib/content";
-import Image from "next/image";
+import { Feature, getFeatures, getHeroContent } from "@/lib/content";
 
 export default function Home() {
   const hero = getHeroContent();
   const features = getFeatures();
-  console.log(features);
 
   return (
     <>
       <h1>{hero.title}</h1>
-      {features.map((f: any) => {
-        return <h2>{f.title}</h2>;
+      {features.map((f: Feature, index) => {
+        return <h2 key={index}>{f.title}</h2>;
       })}
     </>
   );
--- lib/content.ts
+++ lib/content.ts
@@ -1,7 +1,10 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-
+export interface Feature {
+  title: string;
+  description: string;
+}
 export function getHeroContent() {
   const filePath = path.join(process.cwd(), "content/homepage/hero.md");
   const fileContents = fs.readFileSync(filePath, "utf8");
@@ -9,13 +12,31 @@ export function getHeroContent() {
   return data;
 }
 
-export function getFeatures() {
-  const dir = path.join(process.cwd(), "content/homepage/features");
-  const files = fs.readdirSync(dir);
+export interface Feature {
+  slug: string;
+  title: string;
+  description: string;
+}
+
+export function getFeatures(): Feature[] {
+  const featuresDir = path.join(
+    process.cwd(),
+    "content/homepage/features"
+  );
+
+  const files = fs.readdirSync(featuresDir);
 
   return files.map((file) => {
-    const filePath = path.join(dir, file);
-    const content = fs.readFileSync(filePath, "utf-8");
-    return JSON.parse(content);
+    const slug = file.replace(".md", "");
+    const filePath = path.join(featuresDir, file);
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+
+    const { data, content } = matter(fileContent);
+
+    return {
+      slug,
+      title: data.title,
+      description: content.trim(),
+    };
   });
 }
